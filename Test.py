bb538e99f85acdbd41a670d30c0ae2aa28f3270c
BSwainston <bryandswainston@gmail.com>
2024-02-04 09:41:56 -0500

    testing API

--- Test.py
+++ Test.py
@@ -1,3 +1,15 @@
-print("hello world")
+import requests
+import json
+from pprint import pprint
+import pandas as pd
 
-print("new")
+leagueLeader_url = "https://api-web.nhle.com/v1/skater-stats-leaders/current"
+
+team_response = requests.get(leagueLeader_url)
+
+pretty_json = json.loads(team_response.text)
+# print(json.dumps(pretty_json, indent=4))
+
+# fetch players and categories
+SHGoals = pretty_json["goalsSh"][0]["firstName"]["default"]
+print(SHGoals)
